30dadb28ecc039d9457a75600d5d307192a63a4f
salma sbaiti <salmasbaiti1@gmail.com>
2023-01-30 11:53:27 +0100

    basic routing

---
+++ app/settings/chat/page.jsx
@@ -0,0 +1,8 @@
+export default function Page(){
+
+    return(
+        <>
+            <h1> Chat settings page</h1>
+        </>
+    )
+}
---
+++ app/shop/account/page.jsx
@@ -0,0 +1,8 @@
+export default function Page(){
+
+    return(
+        <>
+            <h1> Account page</h1>
+        </>
+    )
+}

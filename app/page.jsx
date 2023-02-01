03ca4daf3abca5aaf60ccef4776e0fdc2e60784b
salma sbaiti <salmasbaiti1@gmail.com>
2023-02-01 21:06:19 +0100

    Loading UI

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,23 @@
 'use client';
 import Link from 'next/link';
+import Loading from './loading';
 import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function Page(){
+    const [loading , setLoading] = useState(true) ; 
     const router = useRouter();
+
+    useEffect(()=>{
+        setTimeout(()=>{ setLoading(false) } , 2000)
+    } , [])
+
+    if(loading){
+        return(<Loading/>)
+    }
+    else{
+
+    
     return(
         <>
             <h1> Learning Next Js framework </h1>
@@ -20,5 +34,5 @@ export default function Page(){
 
             </ul>
         </>
-    )
+    )}
 }
